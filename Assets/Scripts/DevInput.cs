d99c69e703d12cdb37340ea431b92fcf97ebcab0
antij0sh <59179111+antij0sh@users.noreply.github.com>
2020-07-21 12:41:48 -0400

    Player Look and Terrain Added

--- Assets/Scripts/DevInput.cs
+++ Assets/Scripts/DevInput.cs
@@ -6,24 +6,48 @@ public class DevInput : MonoBehaviour
 {
 
     [SerializeField] PlayerMove _playerMove;
+    [SerializeField] PlayerLook _playerLook;
 
     private Vector3 move;
+    private Camera _mainCamera;
     
 
-    // Start is called before the first frame update
     void Start()
     {
         if (_playerMove == null)
         {
             Debug.Log("No player move attached");
         }
+
+        if (_playerLook == null)
+        {
+            Debug.Log("No player look attached");
+        }
+
+        _mainCamera = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
+    {
+        MovePlayer();
+        PlayerLook();
+    }
+
+    private void MovePlayer()
     {
         move.z = Input.GetAxis("Vertical");
         move.x = Input.GetAxis("Horizontal");
         _playerMove.Move(move);
+    }
+
+    private void PlayerLook()
+    {
+        RaycastHit _hit;
+        Ray _ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(_ray, out _hit))
+        {
+            _playerLook.Look(_hit.point);
+            Debug.DrawLine(_playerLook.transform.position, _hit.point, Color.blue);
+        }
     }
 }

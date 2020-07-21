c924ed792011495ac8740ac47ce81ee23fa291a9
antij0sh <59179111+antij0sh@users.noreply.github.com>
2020-07-21 10:44:22 -0400

    Basic Movement

---
+++ Assets/Scripts/DevInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevInput : MonoBehaviour
+{
+
+    [SerializeField] PlayerMove _playerMove;
+
+    private Vector3 move;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_playerMove == null)
+        {
+            Debug.Log("No player move attached");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move.z = Input.GetAxis("Vertical");
+        move.x = Input.GetAxis("Horizontal");
+        _playerMove.Move(move);
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 1f;
+    CharacterController playerCharacter;
+
+    void Awake()
+    {
+        playerCharacter = GetComponent<CharacterController>();
+    }
+
+    public void Move(Vector3 move)
+    {
+        playerCharacter.Move(move * movementSpeed * Time.deltaTime);
+    }
+}

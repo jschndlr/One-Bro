1a54260b3fae6e711acb7f0aeb0cd26dc4df1639
Wagnarok <jbenwagner@gmail.com>
2020-07-22 21:44:01 -0400

    Animation Blend tree
    works with move variable.
    Will need to be based off local leg rotation

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -8,14 +8,18 @@ public class PlayerMove : MonoBehaviour
 {
     [SerializeField] float movementSpeed = 1f;
     CharacterController playerCharacter;
+    Animator animator;
 
     void Awake()
     {
         playerCharacter = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     public void Move(Vector3 move)
     {
+        animator.SetFloat("VelX", move.x);
+        animator.SetFloat("VelZ", move.z);
         playerCharacter.Move(move * movementSpeed * Time.deltaTime);
     }
 }

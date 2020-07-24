b26bf4339ae1a78444b81841ecd589747b0e41f4
antij0sh <59179111+antij0sh@users.noreply.github.com>
2020-07-24 10:16:54 -0400

    Player Look Damping and X vertical look clamping
    Also cleaned up player prefab from previous model

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class PlayerLook : MonoBehaviour
 {
+    public float damping = 0.5f;
+    Vector3 vector = Vector3.zero;
+    Vector3 previousLook;
     public void Look(Vector3 target)
     {
-        transform.LookAt(target);
+        previousLook = transform.position;
+        //disable x rotation
+        target.y = transform.position.y;
+        Vector3 smoothedTarget = Vector3.SmoothDamp(previousLook, target, ref vector, damping);
+        transform.LookAt(smoothedTarget);
+
     }
 }
